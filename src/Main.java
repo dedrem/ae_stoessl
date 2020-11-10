2f98e0c02cfed1f9e23c00de2f1ff10628a562cc
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2020-11-10 11:57:00 +0100

    added aufgabe0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hallo EIT10C");
+        aufgabe0(4,5);
     }
+
+    private static void aufgabe0(double width, double length){
+        // aufgabe0 druckt Fläche und Umfang eines Rechtecks in die Konsole
+        double area = width*length;
+        double circumference = 2*(width+length);
+        System.out.println(String.format("Rechteck: Länge: %s, Breite: %s",length,width));
+        System.out.println(String.format("Fläche: %s, Umfang: %s",area, circumference));
+    }
+
+    private static void aufgabe1(){
+
+    }
+
+    private static void aufgabe2(){
+
+    }
+
+
+
+
 }
