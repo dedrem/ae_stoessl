ac5e036b5897e4bbe0252f5d64281f9d95a6502b
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2020-11-10 12:36:29 +0100

    added aufgabe0-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hallo EIT10C");
+        aufgabe0();
+        aufgabe1();
+        aufgabe2();
+        aufgabe3();
+    }
+
+    public static void aufgabe0(){
+        System.out.println("Hallo Schnellversand GmbH");
+    }
+
+    public static void aufgabe1(){
+        System.out.print("Ist dieses");
+        System.out.print(" Programm eigentlich");
+        System.out.println(" noch "+"lesbar?");
+
+    }
+
+    public static void aufgabe2(){
+        /*Ändere die drei einzelnen Kommentare zu einem
+        mehrzeiligen Kommentar. Schreibe anschließend ein Programm, das "Hallo, ich
+        bin ein Roboter." ausgibt
+        un in einer neuen Zeile "Wie heißt du?" ausgibt.
+         */
+        System.out.println("Hallo, ich bin ein Roboter.");
+        System.out.println("Wie heißt du?");
+    }
+
+    public static void aufgabe3(){
+        System.out.println("Guten Tag");
+        System.out.println("Mein Name ist Donald");
     }
 }
