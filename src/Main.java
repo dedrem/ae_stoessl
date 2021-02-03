838fcb5039a6b05b247f36f6de6c5221b4056a54
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2021-02-03 11:05:38 +0100

    added printer test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hallo EIT10C");
+
+
+        String p = checkPermission(18, false, true) == true ? "die" : "keine";
+        System.out.println(String.format("Der Mitarbeiter erhält %s Erlaubnis",p));
+    }
+
+    public static boolean checkPermission(int age, boolean supervisor, boolean employee){
+        if(employee == false) return false;
+        else if(age >= 18) return true;
+        else if(supervisor == true) return true;
+        else return false;
     }
 }
