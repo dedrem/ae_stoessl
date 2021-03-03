9acfa016c0dbfc478257148bf6ad8fbd23306c30
Maximilian Goldbrunner <maximilian.goldbrunner@googlemail.com>
2021-03-03 11:59:04 +0100

    added a0

---
+++ src/Aufgabe0.java
@@ -0,0 +1,10 @@
+public class Aufgabe0 {
+
+    public Aufgabe0(double start, double end, double increment) {
+
+        while(start<=end){
+            System.out.println(start);
+            start += increment;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hallo EIT10C");
+
+        System.out.println("Aufgabe0");
+        Aufgabe0 aufgabe0 = new Aufgabe0(1,8, 1);
+
+
+
     }
 }
